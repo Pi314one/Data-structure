25c7be5001ff0a1636014577839ef201acbe812b
Pi314one <2540392032@qq.com>
2026-03-10 00:38:49 +0800

    Update 作业1.py

--- 作业1.py
+++ 作业1.py
@@ -7,7 +7,7 @@ class Delete:
         self.iterable=list(iterable)
     def delete(self,index:int)->list[Any]:
         """
-        从索引edit开始，前一个元素替换为后一个
+        从索引index开始，前一个元素替换为后一个
         :param index: 要删除的元素索引
         :return: 变化后列表不输出最后一项
         """
@@ -16,3 +16,4 @@ class Delete:
             self.iterable[i]=self.iterable[i+1]
 
         return list(self.iterable[:n - 1])
+
